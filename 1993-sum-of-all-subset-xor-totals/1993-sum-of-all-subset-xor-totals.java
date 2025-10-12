2e186f750f74936d34b876ed2aa6c53ce3272dd1
bbaspkrrj08 <bbaspkrrj08@gmail.com>
2025-10-13 02:32:35 +0530

    Time: 5 ms (26.97%), Space: 40.9 MB (80.16%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,9 +1,19 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int total=0;
-        for(int num:nums){
-            total |=num;
+        int n = nums.length;
+        int totalSum = 0;
+        int totalSubsets = (int) Math.pow(2, n);
+
+        for (int ansh = 0; ansh < totalSubsets; ansh++) {
+            int currentXor = 0;
+            for (int i = 0; i < n; i++) {
+                if ((ansh & (1 << i)) != 0) {
+                    currentXor = currentXor ^ nums[i];
+                }
+            }
+            totalSum = totalSum + currentXor;
         }
-        return total*(1<<(nums.length-1));
+
+        return totalSum;
     }
 }
