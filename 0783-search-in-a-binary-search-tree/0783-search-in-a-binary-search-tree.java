363bc25e11df596fda1411001d3bacc1fa1d0b93
bbaspkrrj08 <bbaspkrrj08@gmail.com>
2025-10-17 22:56:55 +0530

    Time: 0 ms (100%), Space: 45.1 MB (94.73%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -19,11 +19,11 @@ class Solution {
         return null;
         if(root.val==val)
         return root;
-        if(root.val>val)
+        else if(root.val>val)
         return searchBST(root.left,val);
-        if(root.val<val)
-        return searchBST(root.right,val);
-        return root;
+        else return searchBST(root.right,val);
+        
         
     }
 }
+    
