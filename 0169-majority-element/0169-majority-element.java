f5d0eeef308e3e845b4a5bb0b2dd364203114ea8
bbaspkrrj08 <bbaspkrrj08@gmail.com>
2025-10-28 22:40:23 +0530

    Time: 4 ms (49.69%), Space: 53.3 MB (40.05%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+/*using bit manipulation*/
+class Solution {
+  public int majorityElement(int[] nums) {
+      int akki=0;
+       for(int i= 0; i<32;i++) {
+          int bit=1<<i;
+
+          int count = 0;
+          for(int num:nums) {
+              if((num & bit)!= 0) {
+                  count++;
+              }
+          }
+          if(count>nums.length/2) {
+              akki|=bit;
+          }
+      }
+      return akki;
+  }
+}
