5213623f673be705de425eddfecd596acca66d09
BHUWAN BHASKAR <230343984+bbaspkrrj08@users.noreply.github.com>
2025-12-23 11:42:01 +0530

    Time: 5 ms (32.55%), Space: 51.4 MB (76.66%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    private List<int[]> dir = new ArrayList<>();
+
+    Solution() {
+        dir.add(new int[]{1, 0});
+        dir.add(new int[]{-1, 0});
+        dir.add(new int[]{0, 1});
+        dir.add(new int[]{0, -1});
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
+            return;
+
+        grid[i][j] = '$';
+
+        for (int[] p : dir) {
+            dfs(grid, i + p[0], j + p[1]);
+        }
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0)
+            return 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
