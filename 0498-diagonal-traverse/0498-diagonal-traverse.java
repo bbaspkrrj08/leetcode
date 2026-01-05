63b5099445fac96195349e7cc9f34990be040bdb
BHUWAN BHASKAR <230343984+bbaspkrrj08@users.noreply.github.com>
2026-01-06 04:46:49 +0530

    Time: 14 ms (10.14%), Space: 48.7 MB (6.18%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -0,0 +1,28 @@
+class Solution{
+    public int[] findDiagonalOrder(int[][] mat){
+        int m=mat.length;
+        int n=mat[0].length;
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int key=i+j;
+                map.putIfAbsent(key,new ArrayList<>());
+                map.get(key).add(mat[i][j]);
+            }
+        }
+        List<Integer> resultList=new ArrayList<>();
+        boolean flip=true;
+        for(int k=0;k<=m+n;k++){
+            List<Integer> diagonal=map.get(k);
+            if(diagonal==null) continue;
+            if(flip) Collections.reverse(diagonal);
+            resultList.addAll(diagonal);
+            flip=!flip;
+        }
+        int[] result=new int[resultList.size()];
+        for(int i=0;i<resultList.size();i++){
+            result[i]=resultList.get(i);
+        }
+        return result;
+    }
+}
