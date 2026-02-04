e3f5c4543c0ec5491f592e83f6d4c35c6872643c
BHUWAN BHASKAR <230343984+bbaspkrrj08@users.noreply.github.com>
2026-02-04 11:31:56 +0530

    Time: 49 ms (5.49%), Space: 81.4 MB (95.93%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.java
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.java
@@ -1,21 +1,18 @@
-class Solution {
-    public int numOfSubarrays(int[] arr) {
-        int mod=1000000007;
-        int even=1;
-        int odd=0;
-        int res=0;
-        int prefix=0;
-        for(int num:arr){
-            prefix=prefix+num;
-            if(prefix%2==0){
-                res=res+odd;
-                even++;
-            }else{
-                res=res+even;
-                odd++;
-            }
-            res=res % mod;
-        }
-        return res;
-    }
+class Solution{
+public int numOfSubarrays(int[] arr){
+int mod=1000000007;
+java.util.Map<Integer,Long> map=new java.util.HashMap<>();
+map.put(0,1L);
+long res=0;
+int prefix=0;
+for(int num:arr){
+prefix+=num;
+int parity=prefix%2;
+int need=parity==0?1:0;
+res+=map.getOrDefault(need,0L);
+map.put(parity,map.getOrDefault(parity,0L)+1);
+res%=mod;
+}
+return(int)res;
+}
 }
