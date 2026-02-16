14f7682a71431165e8298852bdf9a6516e903f9a
BHUWAN BHASKAR <230343984+bbaspkrrj08@users.noreply.github.com>
2026-02-16 11:12:37 +0530

    Time: 122 ms (20.72%), Space: 173.3 MB (10.87%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public void convert(TreeNode current, int parent, Map<Integer, List<Integer>> adj) {
+        if (current == null) {
+            return;
+            }
+        if (parent != -1) {
+            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(parent);
+            }
+        if (current.left != null) {
+            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(current.left.val);
+            }
+        if (current.right != null) {
+            adj.computeIfAbsent(current.val, k -> new ArrayList<>()).add(current.right.val);
+            }
+        convert(current.left, current.val, adj);
+        convert(current.right, current.val, adj);
+    }
+    public int amountOfTime(TreeNode root, int start) {
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        convert(root, -1, adj);
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
+        Set<Integer> visited = new HashSet<>();
+        visited.add(start);
+        int minutes = 0;
+        while (!que.isEmpty()) {
+            int n = que.size();
+            while (n-- > 0) {
+                int curr = que.poll();
+                for (int ngbr : adj.getOrDefault(curr, Collections.emptyList())) {
+                    if (!visited.contains(ngbr)) {
+                        que.add(ngbr);
+                        visited.add(ngbr);
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return minutes - 1;
+    }
+}
