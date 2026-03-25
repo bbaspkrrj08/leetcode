ea6059fd853625ec83e46475625fffeab9ac2a99
BHUWAN BHASKAR <230343984+bbaspkrrj08@users.noreply.github.com>
2026-03-25 15:13:38 +0530

    Time: 3 ms (53.63%), Space: 48.5 MB (41.03%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,36 +1,65 @@
 class Solution {
+    private Set<String> set;
+    private List<String>[] memo;
+    private int maxLen;
+    private String s;
+    private int n;
+
     public List<String> wordBreak(String s, List<String> wordDict) {
-        Map<String, List<String>> memo = new HashMap<>();
-        Set<String> dictSet = new HashSet<>(wordDict);
-        return dfs(s, dictSet, memo);
+        this.s = s;
+        this.n = s.length();
+        this.set = new HashSet<>(wordDict);
+        this.memo = new ArrayList[n + 1];
+        this.maxLen = 0;
+        for (String w : wordDict) {
+            maxLen = Math.max(maxLen, w.length());
+        }
+
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && (i - j <= maxLen) && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        if (!dp[n]) {
+            return new ArrayList<>();
+        }
+
+        return dfs(0);
     }
 
-    private List<String> dfs(String s, Set<String> dictSet, Map<String, List<String>> memo) {
-        if (memo.containsKey(s)) {
-            return memo.get(s);
+    private List<String> dfs(int start) {
+        if (memo[start] != null) {
+            return memo[start];
         }
 
         List<String> result = new ArrayList<>();
-        if (s.isEmpty()) {
+        if (start == n) {
             result.add("");
+            memo[start] = result;
             return result;
         }
 
-        for (int i = 1; i <= s.length(); i++) {
-            String prefix = s.substring(0, i);
-            if (dictSet.contains(prefix)) {
-                List<String> suffixes = dfs(s.substring(i), dictSet, memo);
-                for (String suffix : suffixes) {
-                    if (suffix.isEmpty()) {
-                        result.add(prefix);
+        for (int end = start + 1; end <= n && (end - start) <= maxLen; end++) {
+            String word = s.substring(start, end);
+            if (set.contains(word)) {
+                List<String> subResults = dfs(end);
+                for (String sub : subResults) {
+                    if (sub.isEmpty()) {
+                        result.add(word);
                     } else {
-                        result.add(prefix + " " + suffix);
+                        result.add(word + " " + sub);
                     }
                 }
             }
         }
 
-        memo.put(s, result);
+        memo[start] = result;
         return result;
     }
 }
