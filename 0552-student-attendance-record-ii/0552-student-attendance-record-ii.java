8000dcfe3db6e41416f941c0575de644d7895c66
BHUWAN BHASKAR <230343984+bbaspkrrj08@users.noreply.github.com>
2026-04-01 15:03:42 +0530

    Time: 230 ms (63.22%), Space: 206.9 MB (39.87%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.java
@@ -1,40 +1,30 @@
 class Solution {
-    int MOD = 1_000_000_007;
-    Integer[][][] dp;
-    
-    public int checkRecord(int n) {
-        dp = new Integer[n + 1][2][3];
-        return f(0, n, 0, 0);
+public int checkRecord(int n) {
+int MOD = 1_000_000_007;
+long[][][] state = new long[n + 1][2][3];
+    for (int ab = 0; ab < 2; ab++) {
+        for (int l = 0; l < 3; l++) {
+            state[n][ab][l] = 1;
+        }
     }
     
-    private int f(int i, int n, int ab, int l) {
-        
-        if (i >= n) {
-            return 1;
+    for (int i = n - 1; i >= 0; i--) {
+        for (int ab = 0; ab < 2; ab++) {
+            for (int l = 0; l < 3; l++) {
+                long c1 = state[i + 1][ab][0];
+                long c2 = 0;
+                if (ab == 0) {
+                    c2 = state[i + 1][1][0];
+                }
+                long c3 = 0;
+                if (l < 2) {
+                    c3 = state[i + 1][ab][l + 1];
+                }
+                state[i][ab][l] = ((c1 + c2) % MOD + c3) % MOD;
+            }
         }
-        
-        
-        if (dp[i][ab][l] != null) {
-            return dp[i][ab][l];
-        }
-        
-        
-        int c1 = f(i + 1, n, ab, 0);
-        
-        
-        int c2 = 0;
-        if (ab == 0) {
-            c2 = f(i + 1, n, ab + 1, 0);
-        }
-        
-        
-        int c3 = 0;
-        if (l < 2) {
-            c3 = f(i + 1, n, ab, l + 1);
-        }
-        
-        
-        int total = ((c1 + c2) % MOD + c3) % MOD;
-        return dp[i][ab][l] = total;
     }
+    
+    return (int) state[0][0][0];
+}
 }
