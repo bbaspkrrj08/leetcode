703a2df4d22e492693300bb2ca0d0db8aebdd15d
BHUWAN BHASKAR <230343984+bbaspkrrj08@users.noreply.github.com>
2026-04-19 16:23:17 +0530

    Time: 2 ms (29.94%), Space: 47 MB (15.13%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    Map<TreeNode,Integer> map=new HashMap<>();
+    public int rob(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        if(map.containsKey(root)){
+            return map.get(root);
+        }
+        
+        int sum=root.val;
+        
+        if(root.left!=null){
+            sum+=rob(root.left.left);
+            sum+=rob(root.left.right);
+        }
+        
+        if(root.right!=null){
+            sum+=rob(root.right.left);
+            sum+=rob(root.right.right);
+        }
+        
+        int next_sum=rob(root.left)+rob(root.right);
+        
+        int res=Math.max(sum,next_sum);
+        map.put(root,res);
+        return res;
+    }
+}
